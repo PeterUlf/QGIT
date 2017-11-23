480c083e8d82fcab56b12c8e84ccc4489d0e7c2a
Helle Frederiksen <helle0912@gmail.com>
2017-11-23 19:05:43 +0100

    vejledning.php indrykninger etc.

--- vejledning.php
+++ vejledning.php
@@ -1,101 +1,88 @@
 <?php
-        session_start();
-        include "../forbindelse.php";
-$form1="";
-$folder="http://helf-kea.dk/Q2/";
-
-if(!isset($_SESSION["koenavn"]) or $_SESSION["koenavn"]==""){
-  
-}else{
-    $sql="SHOW TABLES LIKE '".$_SESSION["koenavn"]."'";
-    $result=$forbindelse->query($sql); 
-    if($result->num_rows<1){    
-        header('Location: opretQ.php');
+    session_start();
+    include "../forbindelse.php";
+    $form="";
+    $folder="http://helf-kea.dk/Q2/";
+    if(!isset($_SESSION["koenavn"]) or $_SESSION["koenavn"]==""){
+    }else{
+        $sql="SHOW TABLES LIKE '".$_SESSION["koenavn"]."'";
+        $result=$forbindelse->query($sql); 
+        if($result->num_rows<1){    
+            header('Location: opretQ.php');
+        }
     }
-}
-if(isset($_REQUEST["Q"])){
-    $_SESSION["koenavn"]=$_REQUEST["Q"]; 
-    
-    header('Location: vejledning.php');
-}
-
-        function secondsToTime($s){
-            $h = floor($s / 3600);
-            $s -= $h * 3600;
-            $m = floor($s / 60);
-            $s -= $m * 60;
-            return $h.':'.sprintf('%02d', $m).':'.sprintf('%02d', $s);
-        }
-
-        function sletStud($nr, $f){
-            $sql="SELECT navn FROM ".$_SESSION["koenavn"]." WHERE deltagernummer =".$nr;
-            $result=$f->query($sql);
-            if($result->num_rows>0) { 
-                while($row=$result->fetch_assoc()){
-                    $studnavn = $row["navn"];
-                    $_SESSION["studerende"]=$studnavn;
-                    $_SESSION["starttid"]=microtime(true);
-                }
+    if(isset($_REQUEST["Q"])){
+        $_SESSION["koenavn"]=$_REQUEST["Q"]; 
+        header('Location: vejledning.php');
+    }
+    function secondsToTime($s){
+        $h = floor($s / 3600);
+        $s -= $h * 3600;
+        $m = floor($s / 60);
+        $s -= $m * 60;
+        return $h.':'.sprintf('%02d', $m).':'.sprintf('%02d', $s);
+    }
+    function sletStud($nr, $f){
+        $sql="SELECT navn FROM ".$_SESSION["koenavn"]." WHERE deltagernummer =".$nr;
+        $result=$f->query($sql);
+        if($result->num_rows>0) { 
+            while($row=$result->fetch_assoc()){
+                $studnavn = $row["navn"];
+                $_SESSION["studerende"]=$studnavn;
+                $_SESSION["starttid"]=microtime(true);
             }
-            $sql="DELETE FROM ".$_SESSION["koenavn"]." WHERE deltagernummer =".$nr;     
-            $f->query($sql);       
-        } 
-
-
-        function sletmig($f){
-            $sql="DELETE FROM ".$_SESSION["koenavn"]." WHERE tidspunkt ='" . $_SESSION["mig"]."'"; 
-            $f->query($sql);
-            unset($_SESSION["mig"]);
         }
-$output="";
-$fejloutput=""; 
-        
-
-        if( isset( $_REQUEST["navn"] ) ){
-            if(isset($_SESSION["mig"])){
-                 $sql="select * from ".$_SESSION["koenavn"]." where tidspunkt='".$_SESSION["mig"]."'";
-                 $result=$forbindelse->query($sql); 
-                if($result->num_rows==0 AND !isset($_SESSION["pwd"])){
-                    unset($_SESSION["mig"]);
-                }   
-            }
-             
-            if(!isset($_SESSION["mig"]) OR isset($_SESSION["pwd"])){
-                $t=microtime(true);
-                $sql="insert into ".$_SESSION["koenavn"]." (navn, tidspunkt) values('".$_REQUEST['navn']."','".$t."')";
-                $forbindelse->query($sql);
-                
-                $_SESSION["mig"]=$t; 
-                
-                header('Location: vejledning.php');
-            } else {
-                
-                $fejloutput= "Hov, ".$_REQUEST["navn"]."! Du må kun stå i køen 1 gang.". 
-                   "<form class='f-slet'><button name='sletmig' title='slet mig fra køen' value='".$_SESSION['mig']."' type='submit'> Slet mig fra køen </button></a></form>";
-            }
-        }else if(isset($_REQUEST["sletmig"])){
-            sletmig($forbindelse);
-            header('Location: vejledning.php');
-        }else if(isset($_REQUEST["slettes"])){
-            sletStud($_REQUEST["slettes"],$forbindelse);
-            header('Location: vejledning.php');
+        $sql="DELETE FROM ".$_SESSION["koenavn"]." WHERE deltagernummer =".$nr;     
+        $f->query($sql);       
+    } 
+    function sletmig($f){
+        $sql="DELETE FROM ".$_SESSION["koenavn"]." WHERE tidspunkt ='" . $_SESSION["mig"]."'"; 
+        $f->query($sql);
+        unset($_SESSION["mig"]);
+    }
+    $output="";
+    $fejloutput=""; 
+    if( isset( $_REQUEST["navn"] ) ){
+        if(isset($_SESSION["mig"])){
+             $sql="select * from ".$_SESSION["koenavn"]." where tidspunkt='".$_SESSION["mig"]."'";
+             $result=$forbindelse->query($sql); 
+            if($result->num_rows==0 AND !isset($_SESSION["pwd"])){
+                unset($_SESSION["mig"]);
+            }   
         }
-        else if(isset($_REQUEST["logud"])){
-            //unset($_SESSION["vejledernavn"]);
-            unset($_SESSION["pwd"]);
-            unset($_SESSION["studerende"]);
-            unset($_SESSION["starttid"]);
+        if(!isset($_SESSION["mig"]) OR isset($_SESSION["pwd"])){
+            $t=microtime(true);
+            $sql="insert into ".$_SESSION["koenavn"]." (navn, tidspunkt) values('".$_REQUEST['navn']."','".$t."')";
+            $forbindelse->query($sql);
+
+            $_SESSION["mig"]=$t; 
+
             header('Location: vejledning.php');
+        } else {
+
+            $fejloutput= "Hov, ".$_REQUEST["navn"]."! Du må kun stå i køen 1 gang.". 
+               "<form class='f-slet'><button name='sletmig' title='slet mig fra køen' value='".$_SESSION['mig']."' type='submit'> Slet mig fra køen </button></a></form>";
         }
+    }else if(isset($_REQUEST["sletmig"])){
+        sletmig($forbindelse);
+        header('Location: vejledning.php');
+    }else if(isset($_REQUEST["slettes"])){
+        sletStud($_REQUEST["slettes"],$forbindelse);
+        header('Location: vejledning.php');
+    }
+    else if(isset($_REQUEST["logud"])){
+        //unset($_SESSION["vejledernavn"]);
+        unset($_SESSION["pwd"]);
+        unset($_SESSION["studerende"]);
+        unset($_SESSION["starttid"]);
+        header('Location: vejledning.php');
+    }
+    if(isset($_SESSION["pwd"])){
+        $output.="<button onclick='location.href=\"opretQ.php\"'>Ryd kø</button><button onclick='location.href=\"vejledning.php?logud=true\"'>Log ud</button><br>";
 
-        if(isset($_SESSION["pwd"])){
-            $output.="<button onclick='location.href=\"opretQ.php\"'>Ryd kø</button><button onclick='location.href=\"vejledning.php?logud=true\"'>Log ud</button><br>";
-            
-        }            
-        $forbindelse->close();
-
+    }            
+    $forbindelse->close();
 ?>
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -155,21 +142,19 @@ $fejloutput="";
         </style>
     </head>
     <body>
-<!--<div class="wrapper1"><?php echo $form1; ?> </div>-->
         <div class="wrapper">
-            <div><?php echo $form1; ?> </div>
             <h1>Vejledningskø: 
                 <?php 
                     if(isset($_SESSION["koenavn"]) and $_SESSION["koenavn"]!=""){
                         echo $_SESSION["koenavn"]; 
                     }else {
-                        echo "Ikke valgt endnu";
+                        echo "Ingen";
                     }
                 ?>
             </h1>
             <?php if(isset($_SESSION["pwd"])){ ?>
             <div id="urlcontainer">
-                Deltagernes link til køen:<br>
+                Link til kø:<br>
                 <span id="url">
                     <a href='<?php echo $folder; ?>vejledning.php?<?php echo "Q=".$_SESSION["koenavn"]?>'>
                         <?php echo $folder; ?>vejledning.php?<?php echo "Q=".$_SESSION["koenavn"]?>
@@ -181,13 +166,11 @@ $fejloutput="";
             <div id="header"></div>
             <div>I kø: 
                 <div id="under"></div>
-
                 <form class="kinput" >
                     <input type="text" name="navn" placeholder="Navn + emne" required autofocus>
                     <button  type="submit" >i kø</button>
                 </form>
                 <?php if(isset($_SESSION["mig"]))echo $fejloutput; ?>
-
                 <div id="queue"></div>
                 <div id="adm"><?php echo $output; ?><br></div>
             </div>
@@ -195,22 +178,13 @@ $fejloutput="";
         </div>
         <script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
         <script>
-            //$('audio').prop("volume", 0.0005);
-            var koenavn="<?php echo $_SESSION["koenavn"]; ?>";
-            var previous = null;
-            var current=null;
-            
-
             $(document).on("ready", opdater);
             function opdater(){
                 $("#under").load("visO.php").css("display","none");
                 var a = $("#under").html().split("*");
-
-                $("#queue").load("visQ.php");
-                
+                $("#queue").load("visQ.php"); 
             }
             setInterval(opdater, 1000);
-            
         </script>
     </body>
 </html>
